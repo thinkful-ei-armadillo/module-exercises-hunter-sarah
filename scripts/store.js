f8ac0f8d5045de635086c463f9728296d1a8a461
Sarah Drew <sarahdrew93@gmail.com>
2019-01-24 10:31:09 -0600

    added exercise 1

---
+++ scripts/store.js
@@ -0,0 +1,22 @@
+
+
+const store= (function() {
+const foo = 'bar';
+ const items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  let hideCheckedItems = false;
+  let searchTerm ='';
+ 
+    return { 
+    items: items,
+    hideCheckedItems: hideCheckedItems,
+    searchTerm: searchTerm,
+};
+} () );
+
+console.log(store);
+console.log(foo);
